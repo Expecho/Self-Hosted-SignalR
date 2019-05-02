f29a6e1913fae470169948514d50f70ae98624b5
Peter Bons <github@2000miles.nl>
2019-05-02 19:46:25 +0200

    Nuget updates

--- SignalR/SignalR.Client/Program.cs
+++ SignalR/SignalR.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client;
 using Microsoft.AspNet.SignalR.Client.Transports;
 
@@ -6,7 +7,7 @@ namespace SignalR.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Client started");
             
@@ -18,7 +19,7 @@ namespace SignalR.Client
                 hubProxy.On<string, string>("addMessage", (name, message) => Console.WriteLine(message));
 
                 // Start listening
-                connection.Start(new LongPollingTransport()).GetAwaiter().GetResult();
+                await connection.Start(new LongPollingTransport());
 
                 Console.WriteLine("Client connected. Press any key to send a message");
                 Console.ReadKey();
